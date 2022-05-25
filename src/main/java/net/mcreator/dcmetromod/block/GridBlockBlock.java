bd92bc3393b333b5ccb6ed863a1b54404193f023
scriptivity <launchxmusic@gmail.com>
2022-05-25 17:08:00 -0400

    Finished Station Waffle Roof

--- src/main/java/net/mcreator/dcmetromod/block/GridBlockBlock.java
+++ src/main/java/net/mcreator/dcmetromod/block/GridBlockBlock.java
@@ -2,6 +2,9 @@
 package net.mcreator.dcmetromod.block;
 
 import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.IBlockReader;
 import net.minecraft.util.math.BlockPos;
@@ -9,40 +12,55 @@ import net.minecraft.loot.LootContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
 
+import net.mcreator.dcmetromod.itemgroup.DCMetroTabItemGroup;
 import net.mcreator.dcmetromod.DcMetroModModElements;
 
 import java.util.List;
 import java.util.Collections;
 
 @DcMetroModModElements.ModElement.Tag
-public class LogoBlockBlock extends DcMetroModModElements.ModElement {
-	@ObjectHolder("dc_metro_mod:logo_block")
+public class GridBlockBlock extends DcMetroModModElements.ModElement {
+	@ObjectHolder("dc_metro_mod:grid_block")
 	public static final Block block = null;
 
-	public LogoBlockBlock(DcMetroModModElements instance) {
-		super(instance, 4);
+	public GridBlockBlock(DcMetroModModElements instance) {
+		super(instance, 10);
 	}
 
 	@Override
 	public void initElements() {
 		elements.blocks.add(() -> new CustomBlock());
-		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(null)).setRegistryName(block.getRegistryName()));
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(DCMetroTabItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
 	}
 
 	public static class CustomBlock extends Block {
 		public CustomBlock() {
-			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
-			setRegistryName("logo_block");
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.CLOTH).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).notSolid()
+					.setOpaque((bs, br, bp) -> false));
+			setRegistryName("grid_block");
+		}
+
+		@Override
+		public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+			return true;
 		}
 
 		@Override
 		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
-			return 15;
+			return 0;
 		}
 
 		@Override
