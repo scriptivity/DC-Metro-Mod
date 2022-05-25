8ceea96e11f4dfc1af529e75f91a4876f4d5e53a
scriptivity <launchxmusic@gmail.com>
2022-05-24 21:14:41 -0400

    Added Pt1 of Station Roof - Waffle Design (Blocks 1 - 5)

--- src/main/java/net/mcreator/dcmetromod/block/Waffle3Block.java
+++ src/main/java/net/mcreator/dcmetromod/block/Waffle3Block.java
@@ -33,12 +33,12 @@ import java.util.List;
 import java.util.Collections;
 
 @DcMetroModModElements.ModElement.Tag
-public class Waffle1Block extends DcMetroModModElements.ModElement {
-	@ObjectHolder("dc_metro_mod:waffle_1")
+public class Waffle3Block extends DcMetroModModElements.ModElement {
+	@ObjectHolder("dc_metro_mod:waffle_3")
 	public static final Block block = null;
 
-	public Waffle1Block(DcMetroModModElements instance) {
-		super(instance, 1);
+	public Waffle3Block(DcMetroModModElements instance) {
+		super(instance, 3);
 	}
 
 	@Override
@@ -60,7 +60,7 @@ public class Waffle1Block extends DcMetroModModElements.ModElement {
 			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).notSolid()
 					.setOpaque((bs, br, bp) -> false));
 			this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
-			setRegistryName("waffle_1");
+			setRegistryName("waffle_3");
 		}
 
 		@Override
